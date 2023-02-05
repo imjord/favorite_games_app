9599e6a516daf6d20584c0041c7b5ed2d8cbbbab
imjord <dean.jay.goat@gmail.com>
2023-02-04 18:46:08 -0700

    able to open the windows folder on click

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,13 +2,27 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use std::process::Command;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
+#[tauri::command] 
+fn get_games_exe(){
+    // check the users os and open the following folder directory
+    if cfg!(target_os = "windows") {
+        Command::new("explorer").spawn().expect("Failed to open Windows Explorer");
+    } else if cfg!(target_os = "linux") {
+        Command::new("xdg-open").spawn().expect("Failed to open linux explorer (sorry idk the name).");
+    } else if cfg!(target_os = "macos") {
+        Command::new("open").spawn().expect("Failed to open files folder");
+    }
+}
+
+
 
 
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_games_exe])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 const { invoke } = window.__TAURI__.tauri;
 
+let gameArray = [];
+let gameNameEl;
+let gameLauncherEl;
+let openFileEl = document.getElementById("open-file");; 
+
+
+
+function openFile(){
+    invoke("get_games_exe");
+}
+
+
+openFileEl.addEventListener("click", openFile);
